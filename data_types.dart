3b4f571ec1f4e097bae2278f85b4cfddf02892b0
Okinda <Okindameltus00@gmail.com>
2024-03-01 23:21:41 +0300

    data types

---
+++ data_types.dart
@@ -0,0 +1,26 @@
+
+void main() {
+    //integer
+    int age = 25; //Example of an integer data type
+    print('Integer: $age;');
+
+    //double
+    double myDouble = 3.14; //Example of a double data type
+    print('$myDouble');
+
+    //string
+    String myString = 'Hello, Dart'; // Example of a string data type
+    print('$myString');
+
+    //list
+    List<int> myList = [1, 2, 3, 4, 5, 6,]; // Example of a list data type
+    print('$myList');
+
+    //Map
+    Map<String, dynamic> myMap = {
+       'name': 'John',
+       'age': 30,
+       'isStudent': false,
+    }; // Example of a map data type
+    print('$myMap');
+}
